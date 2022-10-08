de004ff47e4ac4717ec88f3e784f7dc5a670e263
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2022-10-08 22:13:29 +0300

    edit API query

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import json
 from functools import wraps
 
 import requests
@@ -22,31 +23,39 @@ def lookup(anime):
     # Contact API
     try:
         # Here we define our query as a multi-line string
-        query = '''
-            query ($id: Int) { # Define which variables will be used in the query (id)
-                Media (id: $id, type: ANIME) { 
-                    # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-                    id
-                    title {
-                    romaji
-                    english
-                    native
+        query = """
+            query ($id: Int, $page: Int, $search: String) {
+                Page (page: $page) {
+                    media (id: $id, search: $search, type: ANIME) {
+                        id
+                        title {
+                            english
+                            romaji
+                        }
+                        desc: description
+                        image: coverImage {
+                            extraLarge
+                        }
+                        episodes
+                        score: averageScore
                     }
                 }
             }
-        '''
+        """
 
         # Define our query variables and values that will be used in the query request
         variables = {
             "search": anime         
         }
 
-        url = 'https://graphql.anilist.co'
+        url = "https://graphql.anilist.co"
 
         # Make the HTTP Api request
         response = requests.post(url, json={'query': query, 'variables': variables})
     except requests.RequestException:
         return None
 
-    response.json()
+    response = response.json()
+    response = response["data"]["Page"]["media"]
+    
     return response
